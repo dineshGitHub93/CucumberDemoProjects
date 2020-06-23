0eec956580f422bcd389fda58f1d59dcef9231cc
dineshGitHub93 <62668115+dineshGitHub93@users.noreply.github.com>
2020-06-23 18:19:11 +0530

    Options in cucumber(dryRun, Strict)

--- Cucumber_Demo/src/main/java/com/cucmberdemo/runner/RunnerClass.java
+++ Cucumber_Demo/src/main/java/com/cucmberdemo/runner/RunnerClass.java
@@ -7,7 +7,18 @@ import cucumber.api.CucumberOptions;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features={"FeatureFiles/loginPagewithExamples.feature"}, 
-glue= {"com.cucumber.stepDefinitions"} )
+glue= {"com.cucumber.stepDefinitions"},
+//dryRun= true,
+strict = true
+
+/**
+ * @author Dinesh
+ * If dryRun is false it will turn on the execution.
+ * It will generate the skeleton for missed feature file.	
+ * dryRun option is restrict the execution and ensure that the feature file is derived properly 
+ * Strict option is executing the program and then check whether the feature file is derived properly
+ */
+)
 public class RunnerClass {
 
 	// it should help us to combine the feature file and the step definition
